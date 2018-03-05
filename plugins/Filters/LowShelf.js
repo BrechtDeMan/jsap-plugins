fa90048b5e7f9ada297bdbad35b10baede90c5d1
Brecht De Man <b.deman@qmul.ac.uk>
2018-03-05 21:18:20 +0100

    Added high and low shelf filter plugins

--- plugins/Filters/LowShelf.js
+++ plugins/Filters/LowShelf.js
@@ -1,9 +1,9 @@
 /*globals BasePlugin */
 /*
-    Peaking
-    A 2nd order biquad peaking filter
+    Low Shelf
+    A 2nd order biquad high shelf filter
 */
-var PeakingFilter = function (factory, owner) {
+var LowShelf = function (factory, owner) {
 
     // This attaches the base plugin items to the Object
     BasePlugin.call(this, factory, owner);
@@ -15,26 +15,24 @@ var PeakingFilter = function (factory, owner) {
         outputNode = this.context.createGain(),
         filter = this.context.createBiquadFilter();
 
-    filter.type = "peaking";
+    filter.type = "lowshelf";
 
     inputNode.connect(filter);
     filter.connect(outputNode);
 
-    var frequency = this.parameters.createNumberParameter("frequency", 1000, 10, 20000);
+    var frequency = this.parameters.createNumberParameter("frequency", 80, 10, 20000);
     var gain = this.parameters.createNumberParameter("gain", 0, -24, +24);
-    var Q = this.parameters.createNumberParameter("Q", 1.414, 0.1, +12);
 
     frequency.bindToAudioParam(filter.frequency);
     gain.bindToAudioParam(filter.gain);
-    Q.bindToAudioParam(filter.Q);
 
     this.addInput(inputNode);
     this.addOutput(outputNode);
 };
 
 // Also update the prototype function here!
-PeakingFilter.prototype = Object.create(BasePlugin.prototype);
-PeakingFilter.prototype.constructor = PeakingFilter;
-PeakingFilter.prototype.name = "PeakingFilter";
-PeakingFilter.prototype.version = "1.0.0";
-PeakingFilter.prototype.uniqueID = "JSPK";
+LowShelf.prototype = Object.create(BasePlugin.prototype);
+LowShelf.prototype.constructor = LowShelf;
+LowShelf.prototype.name = "LowShelf";
+LowShelf.prototype.version = "1.0.0";
+LowShelf.prototype.uniqueID = "JSLS";
